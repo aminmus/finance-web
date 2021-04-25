6bd8713b9fc016ec4892cf40e034a448974a014c
Amin Mushehid <amin.mushehid@gmail.com>
2021-04-25 20:35:12 +0200

    Rename login component

---
+++ src/components/SignIn.tsx
@@ -0,0 +1,69 @@
+import React, { SyntheticEvent, useState } from 'react';
+import { TextField, Button, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useAuth } from '../useAuth';
+
+const useStyles = makeStyles((theme) => ({
+  flexbox: {
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+}));
+
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+  const location = useLocation();
+  const auth = useAuth();
+
+  const { from } = location.state as LocationState || { from: { pathname: '/' } };
+
+  const classes = useStyles();
+
+  async function submitSignIn(event: SyntheticEvent) {
+    event.preventDefault();
+    try {
+      await auth?.signIn(email, password);
+      history.replace(from);
+    } catch (err) {
+      console.error('error caught in submitSignIn: ', err);
+    }
+  }
+
+  return (
+    <form onSubmit={submitSignIn}>
+      <Box display="flex" className={classes.flexbox}>
+        <>
+          <TextField
+            required
+            id="email"
+            label="email"
+            name="email"
+            type="email"
+            autoComplete="username"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            required
+            id="password"
+            label="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit">Sign in</Button>
+        </>
+      </Box>
+    </form>
+  );
+}
+
+type LocationState = {
+  from: Location;
+};
+
+export default SignIn;

2887e19a0e8c8ec97bd609c12d0ea4a5788d5914
Amin Mushehid <amin.mushehid@gmail.com>
2022-12-24 18:40:28 +0100

    Add sign in and sign out link/buttons on main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
-import PrivateRoute from './components/PrivateRoute';
-import SignIn from './components/SignIn';
-import PortfoliosView from './components/PortfoliosView';
 import { ProvideAuth } from './useAuth';
-import { ProvidePortfolios } from './usePortfolios';
-import PortfolioView from './components/PortfolioView';
+import MainSwitch from './components/MainSwitch';
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const history = useHistory();
-
-  // function logout() {
-  //   localStorage.removeItem('accessToken');
-  //   setIsLoggedIn(false);
-  //   history.push('/');
-  // }
-
   return (
     <div className="App">
       <ProvideAuth>
         <Layout>
-          <Switch>
-            <PrivateRoute path="/portfolios/:portfolioId">
-              <ProvidePortfolios>
-                <PortfolioView />
-              </ProvidePortfolios>
-            </PrivateRoute>
-            <PrivateRoute routeOptions={{ exact: true }} path="/portfolios">
-              <ProvidePortfolios>
-                <PortfoliosView />
-              </ProvidePortfolios>
-            </PrivateRoute>
-            <PrivateRoute path="/protected">
-              <p> Yoo we logged in</p>
-            </PrivateRoute>
-            <Route path="/sign-in">
-              <SignIn />
-              {/* <SignIn isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> */}
-            </Route>
-            <Route path="/">
-              <Box display="flex">
-                <p>Main path</p>
-              </Box>
-            </Route>
-          </Switch>
+          <MainSwitch />
         </Layout>
       </ProvideAuth>
     </div>
---
+++ src/components/MainSwitch.tsx
@@ -0,0 +1,55 @@
+import { Box, Button } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { useAuth } from '../useAuth';
+import { ProvidePortfolios } from '../usePortfolios';
+import PortfoliosView from './PortfoliosView';
+import PortfolioView from './PortfolioView';
+import PrivateRoute from './PrivateRoute';
+import SignIn from './SignIn';
+
+function MainSwitch() {
+  const auth = useAuth();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => (auth?.user ? setIsLoggedIn(true) : setIsLoggedIn(false)), [auth?.user]);
+
+  return (
+    <Switch>
+      <PrivateRoute path="/portfolios/:portfolioId">
+        <ProvidePortfolios>
+          <PortfolioView />
+        </ProvidePortfolios>
+      </PrivateRoute>
+      <PrivateRoute routeOptions={{ exact: true }} path="/portfolios">
+        <ProvidePortfolios>
+          <PortfoliosView />
+        </ProvidePortfolios>
+      </PrivateRoute>
+      <PrivateRoute path="/protected">
+        <p> Yoo we logged in</p>
+      </PrivateRoute>
+      <Route path="/sign-in">
+        <SignIn />
+        {/* <SignIn isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> */}
+      </Route>
+      <Route path="/">
+        <Box display="flex">
+          <p>Main path</p>
+          {isLoggedIn
+            ? (
+              <Button onClick={(_e) => auth?.signOut()}>
+                Sign Out
+              </Button>
+            ) : (
+              <Link to="/sign-in">
+                Go to Sign In
+              </Link>
+            )}
+        </Box>
+      </Route>
+    </Switch>
+  );
+}
+
+export default MainSwitch;
